f53e6a073e5b224eaaab2134710eafcc19b838b5
allisonvinicius <allisonvinicius63@gmail.com>
2022-08-14 19:47:17 -0400

    refactory:  update routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { SubjectController } from "./controllers/SubjectController";
 
 const routes = Router();
 
-routes.post("/subject");
+routes.post("/subject", new SubjectController().create);
 
 export default routes;
