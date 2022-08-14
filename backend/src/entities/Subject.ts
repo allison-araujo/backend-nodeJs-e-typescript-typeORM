24c3d0c47801f2b1736a4908756578cb2da33dd7
allisonvinicius <allisonvinicius63@gmail.com>
2022-08-14 19:17:02 -0400

    feat: add table subject and ligation manyTomany whit tables

--- backend/src/entities/Subject.ts
+++ backend/src/entities/Subject.ts
@@ -1,6 +1,30 @@
-import { Entity } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Room } from "./Room";
 
 @Entity("subjects")
 export class Subject {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({ type: "text" })
+  name: string;
+
+  @ManyToMany(() => Room, rooms => rooms.subjects)
+  @JoinTable({
+    name: "room_subject",
+    joinColumn: {
+      name: "room_id",
+      referencedColumnName: "id",
+    },
+    inverseJoinColumn: {
+      name: "subject_id",
+      referencedColumnName: "id",
+    },
+  })
+  rooms: Room[];
 }
